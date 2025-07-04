6b560c19c048cdc051a248380ff1d15fe3cf9ecc
Techron254 <leonardronald04@gmail.com>
2025-07-04 14:45:12 +0300

    add javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+const instagramButton = document.querySelector('#instagram-button');
+const twitterButton = document.querySelector('#twitter-button');
+
+instagramButton.addEventListener('click', () => alert("Sorry, I'm not on Instagram yet. I will update when I do so."));
+twitterButton.addEventListener('click', () => alert("Sorry, I'm not on Twitter yet. I will uodate when I do so."));
