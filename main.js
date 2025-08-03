e7dce62cc65d1205f6649887ad53fb277da937c6
Techron254 <leonardronald04@gmail.com>
2025-08-03 06:00:20 +0300

    make it fully responsive by removing pixel widths and replacing them with percentages. Also added media quaries and reconstructed it with a phone first approach. Also added some cool stylings to enhance aesthetics

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const instagramButton = document.querySelector('#instagram-button');
 const twitterButton = document.querySelector('#twitter-button');
 
-instagramButton.addEventListener('click', () => alert("Sorry, I'm not on Instagram yet. I will update accordingly when I do."));
-twitterButton.addEventListener('click', () => alert("Sorry, I'm not on Twitter yet. I will update accordingly when I do."));
+instagramButton.addEventListener('click', () => alert("Sorry, I'm not on Instagram yet. I will update accordingly when I join."));
+twitterButton.addEventListener('click', () => alert("Sorry, I'm not on Twitter yet. I will update accordingly when I join."));
